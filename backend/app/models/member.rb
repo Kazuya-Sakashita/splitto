80fdb18f046f030fb01dd924498e8df6fa066660
Kazuya-Sakashita <kz0508@gmail.com>
2026-02-17 08:56:39 +0900

    feat: Member作成時にjoined_atを自動設定する

--- backend/app/models/member.rb
+++ backend/app/models/member.rb
@@ -4,8 +4,16 @@ class Member < ApplicationRecord
 
   ROLES = %w[OWNER MEMBER].freeze
 
+  before_validation :ensure_joined_at, on: :create
+
   validates :role, presence: true, inclusion: { in: ROLES }
   validates :active, inclusion: { in: [true, false] }
   validates :joined_at, presence: true
   validates :user_id, uniqueness: { scope: :group_id }
+
+  private
+
+  def ensure_joined_at
+    self.joined_at ||= Time.current
+  end
 end
