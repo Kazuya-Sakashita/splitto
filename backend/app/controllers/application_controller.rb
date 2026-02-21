0d59696810612c0f49c1d2b0657bc23dc82faf4a
Kazuya-Sakashita <kz0508@gmail.com>
2026-02-21 15:22:46 +0900

    Problem Detailsの共通レンダラーを追加する

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
   include ClerkAuthenticatable
+  include ProblemRenderable
 
   before_action :authenticate_with_clerk!
+
+
 end
