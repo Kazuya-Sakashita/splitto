d43fd7d50cfd8b9f5db10e45f8b5ce5d654e6e55
Kazuya-Sakashita <kz0508@gmail.com>
2026-03-06 06:46:13 +0900

    feat(backend): invites ルートを追加

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get "me", to: "me#show"
       resources :groups, only: %i[index create]
+      resources :invites, param: :invite_token, only: [:show]
     end
   end
 end
